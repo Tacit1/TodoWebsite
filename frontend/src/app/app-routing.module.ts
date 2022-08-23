db7c35573556da0451d8cac8ddb3f6b69119ad5a
Helene_Rinn <helene.rinn@web.de>
2022-08-23 12:08:07 +0200

    navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalendarComponent} from "./calendar/calendar.component";
+import {ToDoComponent} from "./to-do/to-do.component";
+import {HabitsComponent} from "./habits/habits.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Calendar', component: CalendarComponent}, //Routing für Navbar Module
+  { path: 'ToDo', component: ToDoComponent},
+  { path: 'Habits', component: HabitsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from "@angular/material/icon";
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { HabitsComponent } from './habits/habits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CalendarComponent,
+    ToDoComponent,
+    HabitsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
