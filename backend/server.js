bc3b0cafc6380a696ccb95fe57c587137f609f12
Helene_Rinn <helene.rinn@web.de>
2022-08-28 14:27:40 +0200

    m

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,24 @@ const PORT = 3000;
 app.use(express.json());                // alle Responses die wir bekommen, wandeln wir in JSON um
 app.use('/', routes);                   // hier das was man oben als Datei eingebunden hat
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // connect to mongoDB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('mongodb://127.0.0.1:27017/members', { useNewUrlParser: true, useUnifiedTopology: true });
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from "@angular/material/icon";
-import {ScheduleModule} from "@syncfusion/ej2-angular-schedule";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { ScheduleModule } from "@syncfusion/ej2-angular-schedule";
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ToDoComponent } from './to-do/to-do.component';
 import { HabitsComponent } from './habits/habits.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HabitsComponent } from './habits/habits.component';
     NavbarComponent,
     CalendarComponent,
     ToDoComponent,
-    HabitsComponent
+    HabitsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
